429277a3d1e2a5b5cd017db31910374cb888d544
FedeFreire <bandoleir@gmail.com>
2024-02-25 17:32:23 -0500

    Final Adjustments for ticket 2

--- src/components/pages/DataVisualizations/GraphWrapper.jsx
+++ src/components/pages/DataVisualizations/GraphWrapper.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import CitizenshipMapAll from './Graphs/CitizenshipMapAll';
@@ -15,12 +15,6 @@ import ScrollToTopOnMount from '../../../utils/scrollToTopOnMount';
 
 const { background_color } = colors;
 
-/*
-- GraphWrapper is a functional component, it allows the use of useEffect and useParams
-- It receives the props:
-      - dispatch: provided by Redux connect, allows to send actions to the store
-      - set_view: updates the state to reflect the type of graphic displayed 
-      */
 function GraphWrapper(props) {
   const { set_view, dispatch } = props;
   let { office, view } = useParams();
@@ -29,27 +23,33 @@ function GraphWrapper(props) {
     view = 'time-series';
   }
   let map_to_render;
-  // map_to_render is a variable that will store the component to be rendered, depending on the view and office parameters.
 
   if (!office) {
-    map_to_render =
-      view === 'time-series' ? (
-        <TimeSeriesAll />
-      ) : view === 'office-heat-map' ? (
-        <OfficeHeatMap />
-      ) : (
-        view === 'citizenship' && <CitizenshipMapAll />
-      );
+    switch (view) {
+      case 'time-series':
+        map_to_render = <TimeSeriesAll />;
+        break;
+      case 'office-heat-map':
+        map_to_render = <OfficeHeatMap />;
+        break;
+      case 'citizenship':
+        map_to_render = <CitizenshipMapAll />;
+        break;
+      default:
+        break;
+    }
   } else {
-    map_to_render =
-      view === 'time-series' ? (
-        <TimeSeriesSingleOffice office={office} />
-      ) : (
-        view === 'citizenship' && <CitizenshipMapSingleOffice office={office} />
-      );
+    switch (view) {
+      case 'time-series':
+        map_to_render = <TimeSeriesSingleOffice office={office} />;
+        break;
+      case 'citizenship':
+        map_to_render = <CitizenshipMapSingleOffice office={office} />;
+        break;
+      default:
+        break;
+    }
   }
-  // I decided to change from "switch statement" to "ternary operator" because it is more concise and readable. Also, it is easier to maintain
-  // due to the small number of conditions.
 
   const updateStateWithNewData = async (
     years,
@@ -59,35 +59,22 @@ function GraphWrapper(props) {
   ) => {
     const baseUrl = 'https://hrf-asylum-be-b.herokuapp.com/cases';
     const params = { from: years[0], to: years[1], office };
-    /*
-- The updateStateWithNewData function is an async function that receives the years, view, office, and stateSettingCallback as parameters.
-- It makes a request to the API to get the data for the selected years, view, and office. Then, it updates the state with the new data.
-- I took this approach to avoid repeating the same code in the different components because they all need to fetch the data from the API.
-*/
+
     try {
       const [fiscal, citizenship] = await Promise.all([
         axios.get(`${baseUrl}/fiscalSummary`, { params }),
         axios.get(`${baseUrl}/citizenshipSummary`, { params }),
       ]);
-      /*
-- The constants fiscal and citizenship are assigned the result of the Promise.all method, which is an array of the responses of the requests to the API.
-- I used the Promise.all method to make the requests to the API in parallel, this way the requests are made at the same time and the response time is reduced.
-- The requests are made to the fiscalSummary and citizenshipSummary endpoints of the API, passing the years and office as parameters.
-- The response is stored in the fiscal and citizenship variables.
-- I used await to wait for the response of both requests before continuing with the execution of the code.
-*/
+
       fiscal.data['citizenshipResults'] = citizenship.data;
       stateSettingCallback(view, office, [fiscal.data]);
     } catch (error) {
       console.error('Error fetching data:', error);
     }
   };
-  /*
-- The fiscal and citizenship data are combined into a single object, which is then passed to the stateSettingCallback function.
-- The stateSettingCallback function is called with the view, office, and the combined fiscal and citizenship data as parameters.
-*/
 
-  const clearQuery = () => dispatch(resetVisualizationQuery(view, office));
+  const clearQuery = (view, office) =>
+    dispatch(resetVisualizationQuery(view, office));
 
   return (
     <div
